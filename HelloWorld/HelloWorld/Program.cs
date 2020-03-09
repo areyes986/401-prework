4e06b6a1c89f91e46d1099a97fde74a95e9e0626
unknown <areyes986@gmail.com>
2020-03-09 00:17:24 -0700

    practicing how to push

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+}
+
+// main is method, first method was app is executed.
+// class called program, container for all of methods of app
+//namespace called helloWorld, 
