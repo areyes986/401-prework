00018a5c3e5839e969bd4b0f0b427716507cd63d
unknown <areyes986@gmail.com>
2020-03-09 02:10:40 -0700

    else/if statements practice

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Console.WriteLine("Allys giveaway!");
+            Console.Write("Choose a door: 1,2 or 3:");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+                 message = "You won a new car!";
+            else if (userValue == "2")          
+                message = "You won a boat!";           
+            else if (userValue == "3")           
+                message = "You won a new cat!";          
+            else
+            {
+                message = "Sorry you lose.";
+                //message = message + ":)"; 
+                message += ":D";
+            }
+            
+            Console.WriteLine(message);
             Console.ReadLine();
+            */
+
+            Console.WriteLine("Allys giveaway!");
+            Console.Write("Choose a door: 1,2 or 3:");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            string message1 = "fool";
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+            //Console.WriteLine("You won a {0}.", message);
+            Console.WriteLine("You entered: {0}, therefore, You won a {1}. {2}", userValue, message, message1);
+
+            Console.ReadLine();
+
+
         }
     }
 
@@ -16,3 +53,5 @@ namespace HelloWorld
 // main is method, first method was app is executed.
 // class called program, container for all of methods of app
 //namespace called helloWorld, 
+
+//  = is an assignment operator , == evaluation if true or false
