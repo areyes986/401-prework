e0e8c3f3f9664c3fa2727edf18f7ec3c0407e79e
unknown <areyes986@gmail.com>
2020-03-09 20:05:25 -0700

    practing variables

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,66 @@
 ﻿using System;
 
+
+
+    // IF STATEMENTS
+//namespace HelloWorld
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            /*
+//            Console.WriteLine("Allys giveaway!");
+//            Console.Write("Choose a door: 1,2 or 3:");
+//            string userValue = Console.ReadLine();
+
+//            string message = "";
+
+//            if (userValue == "1")
+//                 message = "You won a new car!";
+//            else if (userValue == "2")          
+//                message = "You won a boat!";           
+//            else if (userValue == "3")           
+//                message = "You won a new cat!";          
+//            else
+//            {
+//                message = "Sorry you lose.";
+//                //message = message + ":)"; 
+//                message += ":D";
+//            }
+
+//            Console.WriteLine(message);
+//            Console.ReadLine();
+//            */
+
+//            Console.WriteLine("Allys giveaway!");
+//            Console.Write("Choose a door: 1,2 or 3:");
+//            string userValue = Console.ReadLine();
+
+//            string message = (userValue == "1") ? "boat" : "strand of lint";
+//            //Console.Write("You won a ");
+//            //Console.Write(message);
+//            //Console.Write(".");
+//            //Console.WriteLine("You won a {0}.", message);
+//            Console.WriteLine("You entered: {0}, therefore, You won a {1}. {2}", userValue, message);
+
+//            Console.ReadLine();
+
+
+//        }
+//    }
+
+//}
+
+// main is method, first method was app is executed.
+// class called program, container for all of methods of app
+//namespace called helloWorld, 
+
+//  = is an assignment operator , == evaluation if true or false
+
+
+
+    // VARIABLES
 namespace HelloWorld
 {
     class Program
@@ -7,51 +68,26 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             /*
-            Console.WriteLine("Allys giveaway!");
-            Console.Write("Choose a door: 1,2 or 3:");
-            string userValue = Console.ReadLine();
+            int x;
+            int y;
 
-            string message = "";
+            x = 7;
+            y = x + 3;
 
-            if (userValue == "1")
-                 message = "You won a new car!";
-            else if (userValue == "2")          
-                message = "You won a boat!";           
-            else if (userValue == "3")           
-                message = "You won a new cat!";          
-            else
-            {
-                message = "Sorry you lose.";
-                //message = message + ":)"; 
-                message += ":D";
-            }
-            
-            Console.WriteLine(message);
+            Console.WriteLine(y);
             Console.ReadLine();
             */
 
-            Console.WriteLine("Allys giveaway!");
-            Console.Write("Choose a door: 1,2 or 3:");
-            string userValue = Console.ReadLine();
+            Console.WriteLine("What is your name?");
 
-            string message = (userValue == "1") ? "boat" : "strand of lint";
-            string message1 = "fool";
-            //Console.Write("You won a ");
-            //Console.Write(message);
-            //Console.Write(".");
-            //Console.WriteLine("You won a {0}.", message);
-            Console.WriteLine("You entered: {0}, therefore, You won a {1}. {2}", userValue, message, message1);
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
 
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + "!");
             Console.ReadLine();
-
-
         }
     }
-
 }
-
-// main is method, first method was app is executed.
-// class called program, container for all of methods of app
-//namespace called helloWorld, 
-
-//  = is an assignment operator , == evaluation if true or false
