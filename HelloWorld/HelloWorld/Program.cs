b855e72f1906ff0b2cf6e6d9fbac28b1ee5dddd0
unknown <areyes986@gmail.com>
2020-03-12 00:09:48 -0700

    notes for prework

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -471,3 +471,18 @@ namespace HelloWorld
     }
 }
 
+
+// NOTES
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+// Output: The range of integers is -2147483648 to 2147483647
